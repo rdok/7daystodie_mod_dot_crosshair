823de67ad8fed726c8611408a688becdac48f0ee
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-03 13:51:54 +0100

    refactor: CleanCode: single responsibility principle.

---
+++ DotCrosshair/Harmony/Init.cs
@@ -0,0 +1,15 @@
+﻿namespace DotCrosshair.Harmony
+{
+    public class Init : IModApi
+    {
+        private static readonly ILogger _logger = new Logger();
+
+        public void InitMod(Mod modInstance)
+        {
+            _logger.Info("InitMod: Loading Dot Crosshair Mod");
+            var harmony = new HarmonyLib.Harmony("uk.co.rdok.7daystodie.mod.dotcrosshair");
+            harmony.PatchAll();
+            _logger.Info("InitMod: Dot Crosshair Mod loaded successfully.");
+        }
+    }
+}

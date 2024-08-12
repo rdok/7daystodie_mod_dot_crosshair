b18a510a5d18f2cfe4bb65027333dd299bcfc4ac
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-12 23:05:13 +0100

    feat: Single responsible of mod: render dot.

--- DotCrosshair/Harmony/Patch.cs
+++ DotCrosshair/Harmony/Patch.cs
@@ -12,21 +12,9 @@ namespace DotCrosshair.Harmony
 
         public static bool Prefix(EntityPlayerLocal __instance, NGuiWdwInGameHUD _guiInGame, bool bModalWindowOpen)
         {
-            var holdingItem = __instance.inventory.holdingItemItemValue;
-            var repairToolTags = new[] { "tool", "harvestingSkill", "knife", "perkBrawler" };
-            var holdingTool = holdingItem?.ItemClass?.Actions?
-                .Any(action => HasTags(action, repairToolTags, TagCheckType.Any)) ?? false;
-
-#if MAIN_VERSION || DEBUG
-            const bool callGuidDrawCrosshairFunction = true;
-            if (!holdingTool)
-            {
-                return callGuidDrawCrosshairFunction;
-            }
-#endif
             const bool disableGuiDrawCrosshairFunction = false;
 
-            if (!_guiInGame.showCrosshair || Event.current.type != EventType.Repaint ||
+            if (Event.current.type != EventType.Repaint ||
                 __instance.IsDead() ||
                 __instance.emodel.IsRagdollActive || __instance.AttachedToEntity != null)
             {
@@ -38,50 +26,12 @@ namespace DotCrosshair.Harmony
                 return disableGuiDrawCrosshairFunction;
             }
 
-            DotCrosshair.Draw(__instance);
+            if (_guiInGame.showCrosshair)
+            {
+                DotCrosshair.Draw(__instance);
+            }
 
             return disableGuiDrawCrosshairFunction;
-        }
-
-        private enum TagCheckType
-        {
-            Any,
-            All
-        }
-
-        private static bool HasTags([CanBeNull] ItemAction _itemAction, string[] tagNames, TagCheckType checkType)
-        {
-            if (_itemAction?.item == null) return false;
-
-            if (!_itemAction.item.Properties.Values.ContainsKey("Tags"))
-            {
-                Logger.Info("HasAnyTag: No tags in _itemAction.item.Properties.Values");
-                return false;
-            }
-
-            var tags = _itemAction.item.Properties.Values["Tags"];
-            Logger.Info($"HasAnyTag: Item Tags: {tags}");
-
-            bool hasTags;
-
-            switch (checkType)
-            {
-                case TagCheckType.Any:
-                    hasTags = tagNames.Any(tagName => tags.Contains(tagName));
-                    break;
-                case TagCheckType.All:
-                    hasTags = tagNames.All(tagName => tags.Contains(tagName));
-                    break;
-                default:
-                    Logger.Info("HasTags: Invalid check type");
-                    return false;
-            }
-
-            Logger.Info(hasTags
-                ? $"The item has {checkType.ToString().ToLower()} of the specified tags."
-                : $"The item does not have {checkType.ToString().ToLower()} of the specified tags.");
-
-            return hasTags;
         }
     }
 }

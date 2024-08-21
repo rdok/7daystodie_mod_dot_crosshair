452b4e33cff3d637186ffa35745ce63063422a28
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-21 22:00:35 +0100

    fix: Compatibility with other mods that hide the crosshair.

--- DotCrosshair/Harmony/Patch.cs
+++ DotCrosshair/Harmony/Patch.cs
@@ -1,3 +1,4 @@
+using System;
 using HarmonyLib;
 using UnityEngine;
 
@@ -10,14 +11,38 @@ namespace DotCrosshair.Harmony
         {
             const bool disableGuiDrawCrosshairFunction = false;
 
-            if (Event.current.type != EventType.Repaint ||
+            if (!_guiInGame.showCrosshair ||
+                Event.current.type != EventType.Repaint ||
                 __instance.IsDead() ||
-                __instance.emodel.IsRagdollActive || __instance.AttachedToEntity != null)
+                __instance.emodel.IsRagdollActive ||
+                __instance.AttachedToEntity != null)
+            {
+                Debug.Log("[DotCrosshair] Crosshair not drawn due to invalid player state.");
                 return disableGuiDrawCrosshairFunction;
+            }
 
-            if (bModalWindowOpen || __instance.inventory == null) return disableGuiDrawCrosshairFunction;
+            if (bModalWindowOpen || __instance.inventory == null)
+            {
+                Debug.Log("[DotCrosshair] Crosshair not drawn due to modal window open or inventory null.");
+                return disableGuiDrawCrosshairFunction;
+            }
 
-            if (_guiInGame.showCrosshair) DotCrosshair.Draw();
+            var crosshairType =
+                __instance.inventory.holdingItem.GetCrosshairType(__instance.inventory.holdingItemData);
+
+            if (crosshairType == ItemClass.EnumCrosshairType.Crosshair ||
+                crosshairType == ItemClass.EnumCrosshairType.CrosshairOnAiming)
+            {
+                if (crosshairType == ItemClass.EnumCrosshairType.Crosshair
+                    && __instance.AimingGun
+                    && !ItemAction.ShowDistanceDebugInfo)
+                {
+                    return disableGuiDrawCrosshairFunction;
+                }
+            }
+
+            Debug.Log("[DotCrosshair] Drawing custom crosshair.");
+            DotCrosshair.Draw(__instance);
 
             return disableGuiDrawCrosshairFunction;
         }

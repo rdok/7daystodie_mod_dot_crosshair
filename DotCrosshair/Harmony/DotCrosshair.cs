2d3d0872ca8e92b2a7bf8b5c3bbe005a8e430d07
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-21 20:08:01 +0100

    feat: Change crosshair shape depending on selected.

--- DotCrosshair/Harmony/DotCrosshair.cs
+++ DotCrosshair/Harmony/DotCrosshair.cs
@@ -6,6 +6,8 @@ namespace DotCrosshair.Harmony
     {
         private static readonly ILogger Logger = new Logger();
 
+        public static DotShape Shape { get; set; } = DotShape.Round;
+
         public static float DotSize { get; set; } = 5f;
         public static byte RedColour { get; set; } = 255;
         public static byte GreenColour { get; set; } = 255;
@@ -17,33 +19,21 @@ namespace DotCrosshair.Harmony
         public static float ShadowOffsetX { get; set; } = .5f;
         public static float ShadowOffsetY { get; set; } = 1.5f;
 
-        public static void Draw(EntityPlayerLocal player)
+        public static void Draw()
         {
             var center = new Vector2(Screen.width / 2, Screen.height / 2);
-
-
-            var dotRect = new Rect(center.x - DotSize / 2, center.y - DotSize / 2, DotSize, DotSize);
-
-            if (ShadowEnabled)
-            {
-                var shadowOffset = new Vector2(ShadowOffsetX, ShadowOffsetY);
-                var shadowRect = new Rect(
-                    dotRect.x + shadowOffset.x,
-                    dotRect.y + shadowOffset.y,
-                    DotSize + ShadowSize,
-                    DotSize + ShadowSize
-                );
-                var fullyOpaqueBlack = new Color(0f, 0f, 0f, 0.5f);
-                GUI.color = fullyOpaqueBlack;
-                GUI.DrawTexture(shadowRect, Texture2D.whiteTexture);
-            }
-
             var alphaChannel = (byte)(AlphaChannel * 255);
-            
             var crosshairColor = new Color32(RedColour, GreenColour, BlueColour, alphaChannel);
 
-            GUI.color = crosshairColor;
-            GUI.DrawTexture(dotRect, Texture2D.whiteTexture);
+            if (Shape.IsSquare())
+            {
+                SquareDot.Render(
+                    center, DotSize, crosshairColor, ShadowEnabled, ShadowSize, ShadowOffsetX, ShadowOffsetY
+                );
+                return;
+            }
+
+            RoundDot.Render(center, DotSize, crosshairColor);
         }
     }
 }
